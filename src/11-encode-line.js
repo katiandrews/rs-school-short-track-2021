106773aae500a61293147c781686106053e2f3a8
katiandrews <andryushina.eka@gmail.com>
2021-04-08 01:40:22 +0200

    feat: add solution of 11-encode-line.js

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,24 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    let sum = 0;
+    if (str[i] === str[i + 1] || str[i] === str[i - 1]) {
+      for (let j = 0; j < str.length; j++) {
+        if (str[i] === str[j]) {
+          sum++;
+        }
+      }
+      if (str[i] === str[i - 1] && str[i] !== str[i + 1]) {
+        result += sum + str[i];
+      }
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
 }
 
 module.exports = encodeLine;
