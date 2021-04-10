d4b1f235d67f8002013fad1c85cf315a0668e1e6
katiandrews <andryushina.eka@gmail.com>
2021-04-10 22:56:34 +0200

    feat: add solution of 04-mine-sweeper.js

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,46 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const matrixCopy = [];
+  // create copy of original array;
+  for (let i = 0; i < matrix.length; i++) {
+    matrixCopy[i] = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      matrixCopy[i].push(matrix[i][j]);
+    }
+  }
+  // check if there is a true around the element;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      let counter = 0;
+      for (let n = 0; n < 2; n++) {
+        if (n === 0) {
+          if (matrix[i][j - 1] !== undefined && matrix[i][j - 1] === true) {
+            counter++;
+          }
+          if (matrix[i][j + 1] !== undefined && matrix[i][j + 1] === true) {
+            counter++;
+          }
+        } else {
+          for (let x = -1; x < 2; x++) {
+            if (matrix[i - n]) {
+              if (matrix[i - n][j - x] !== undefined && matrix[i - n][j - x] === true) {
+                counter++;
+              }
+            }
+            if (matrix[i + n]) {
+              if (matrix[i + n][j - x] !== undefined && matrix[i + n][j - x] === true) {
+                counter++;
+              }
+            }
+          }
+        }
+      }
+      matrixCopy[i][j] = counter;
+    }
+  }
+  return matrixCopy;
 }
 
 module.exports = minesweeper;
